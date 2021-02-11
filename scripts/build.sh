a090eacdd4f90258ae0cfe9c5476c10abed085a7
ydotlai <y.lai@163.com>
2021-02-11 16:53:54 +0800

    updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-rm -rf cm* CM* lib* Testing* tests* include tests  KCBox
+rm -rf cm* CM* lib* Testing* tests* include tests compile_commands.json Makefile KCBox
 cmake -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j4
+rm -rf cm* CM* lib* Testing* tests* include tests compile_commands.json Makefile
