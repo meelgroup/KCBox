0efe5b525adcfa806f6987da499c8048207dbc8a
ydotlai <y.lai@163.com>
2023-05-22 13:27:39 +0800

    static compilation script

---
+++ scripts/build_static.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+rm -rf cmake* CMake* lib* Testing* tests* include tests compile_commands.json KCBox PreLite Panini ExactMC PartialKC
+toolname=KCBox
+if [ $# != 0 ] ; then
+	toolname=$1
+fi
+cmake -DSTATICCOMPILE=ON -DENABLE_PYTHON_INTERFACE=ON -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DTOOLNAME=${toolname} ..
+make -j6
+rm -rf cmake* CMake* lib* Testing* tests* include tests compile_commands.json Makefile
